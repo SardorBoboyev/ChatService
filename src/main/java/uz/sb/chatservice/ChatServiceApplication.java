d51361721f879132ad333e12d1404deda534a897
raykhonaNorboeva <raykhonanorboeva050@gmail.com>
2024-10-07 13:09:22 +0500

    settings

--- src/main/java/uz/sb/chatservice/ChatServiceApplication.java
+++ src/main/java/uz/sb/chatservice/ChatServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @EnableJpaAuditing(auditorAwareRef = "customAuditor")
 @EnableFeignClients
 public class ChatServiceApplication {
--- src/main/java/uz/sb/chatservice/repository/ChatRepository.java
+++ src/main/java/uz/sb/chatservice/repository/ChatRepository.java
@@ -11,7 +11,9 @@ public interface ChatRepository extends JpaRepository<ChatEntity, Long> {
 
     boolean existsByUser1IdAndUser2Id(Long user1Id, Long user2Id);
 
-    @Query("SELECT c FROM chats c WHERE c.user1Id = :userId OR c.user2Id = :userId")
-    List<ChatEntity> findAllChatsByUserId(@Param("userId") Long userId);
+    @Query("SELECT c FROM chats c WHERE " +
+            "((c.user1Id = :userId AND c.isDeletedByUser1 = false) OR " +
+            "(c.user2Id = :userId AND c.isDeletedByUser2 = false))")
+    List<ChatEntity> findAllByUser(@Param("userId") Long userId);
 
 }
