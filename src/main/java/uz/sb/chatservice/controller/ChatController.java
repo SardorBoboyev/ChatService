5b0939dfc41e390bd1b75beb2be08219058d803c
raykhonaNorboeva <raykhonanorboeva050@gmail.com>
2024-10-09 16:04:47 +0500

    lib

--- src/main/java/uz/sb/chatservice/controller/ChatController.java
+++ src/main/java/uz/sb/chatservice/controller/ChatController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import uz.sb.chatservice.domain.entity.ChatEntity;
 import uz.sb.chatservice.service.ChatService;
 import uz.sb.domain.dto.request.ChatRequest;
+import uz.sb.domain.dto.request.DeletedChatRequest;
 
 
 import java.util.List;
