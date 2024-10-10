77cd35d27f95ac04729f33142890c5b7b8dc838d
SardorBoboyev <boboyevsardor350@gmail.com>
2024-10-10 11:09:25 +0500

    ChatService changed with library

--- src/main/java/uz/sb/chatservice/service/ChatServiceImpl.java
+++ src/main/java/uz/sb/chatservice/service/ChatServiceImpl.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uz.sb.chatservice.clients.AuthServiceClient;
 import uz.sb.chatservice.domain.entity.ChatEntity;
+import uz.sb.chatservice.exception.DataNotFoundException;
 import uz.sb.chatservice.repository.ChatRepository;
 import uz.sb.domain.dto.request.ChatRequest;
 import uz.sb.domain.dto.request.DeletedChatRequest;
@@ -25,11 +26,11 @@ public class ChatServiceImpl implements ChatService {
         Long user2Id = chatRequest.getUser2Id();
 
         if (chatRepository.existsByUser1IdAndUser2Id(user1Id, user2Id)) {
-            throw new RuntimeException("chat already exists");
+            throw new DataNotFoundException("chat already exists");
         }
 
         if (Objects.isNull(authServiceClient.findById(user1Id)) || Objects.isNull(authServiceClient.findById(user2Id))) {
-            throw new RuntimeException("users not found");
+            throw new DataNotFoundException("users not found");
         }
         return chatRepository.save(ChatEntity.builder()
                 .user1Id(user1Id)
@@ -40,7 +41,7 @@ public class ChatServiceImpl implements ChatService {
 
     @Override
     public ChatEntity findById(Long chatId) {
-        return chatRepository.findById(chatId).orElseThrow(() -> new RuntimeException("chat not found"));
+        return chatRepository.findById(chatId).orElseThrow(() -> new DataNotFoundException("chat not found"));
     }
 
 
